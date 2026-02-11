d744e52ca2fd34de56103c70b98958f5402df06d
Ordenion <tytytt999@gmail.com>
2026-02-11 12:09:36 +0200

    fixed the tokens names

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  envPrefix: ['VITE_', 'FIREBASE_'],
   plugins: [react()],
   resolve: {
     alias: {
